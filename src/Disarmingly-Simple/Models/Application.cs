c18bb394e722ada94ebf1831fc4d97fc8a9a235c
David Wilson <davidtheadmiral@gmail.com>
2017-08-29 18:30:09 -0700

    add basic functionality for font size and color change, need to polish font size adjustment and ensure font color change works

--- src/Disarmingly-Simple/Models/Application.cs
+++ src/Disarmingly-Simple/Models/Application.cs
@@ -16,6 +16,8 @@ namespace Disarmingly_Simple.Models
         public SpeechRecognitionEvent result;
         public string[] bgToGreen = { "change", "the", "background", "color", "to", "green" };
         public string[] bgToRed = { "change", "the", "background", "color", "to", "red" };
+		public string[] colors = { "red", "green", "blue", "yellow" };
+		public string[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty" };
 
         private SpeechToTextService _speechToText = new SpeechToTextService();
 
@@ -59,6 +61,7 @@ namespace Disarmingly_Simple.Models
                 {
                     words = speechResult.Results[i].Alternatives[0].Transcript.Split(' ');//fix array splitting and adding to words array. 
                 }
+				//NEED TO ADD ERROR HANDLING FOR TO/TWO AND OTHER SIMILAR ISSUES
             }
             else
             {
@@ -69,7 +72,7 @@ namespace Disarmingly_Simple.Models
         }
         public Array splitArrayTesting(string[] chosenArray)
         {
-            string[] wordToRemove = { "the", "of", "it", "to" };
+            string[] wordToRemove = { "the", "of", "it", "to", "two" };
             string[] fixedArray = { };
             //string[] codeToReturn = { };
             for(var i = 0; i < wordToRemove.Count(); i ++)
@@ -108,7 +111,48 @@ namespace Disarmingly_Simple.Models
                 }
                 return fixedArray;
             }
-            return fixedArray;
+			else if( fixedArray.Contains("font"))
+			{
+				if(fixedArray.Contains("size"))
+				{
+					foreach(var number in numbers)
+					{
+						if(fixedArray.Contains(number))
+						{
+							
+							string[] codeToReturn = { "<style>body{font-size:"+(Array.IndexOf(numbers, number)+1)+"px;}</style>", number };
+							return codeToReturn;
+						}
+					}
+					string[] toReturn = { "", "Failure in font size"};
+					return toReturn;
+				}
+				else if(fixedArray.Contains("color"))
+				{
+					foreach(string color in colors)
+					{
+						if(fixedArray.Contains(color))
+						{
+							string[] codeToReturn = { "<style>body{color:"+color+";}</style>}", "changed font color to "+color};
+						}
+						else
+						{
+							string[] codeToReturn = { "", "Incapable of returning this color yet!  ..Oh yeah beep and boop.. still a abot" };
+						}
+					}
+					string[] toReturn = { "", "Failure in font color" };
+					return toReturn;
+				}
+				else
+				{
+					string[] toReturn = { "", "Failure in font" };
+					return toReturn;
+				}
+			}
+			else
+			{
+				return fixedArray;
+			}
         }
     }
 }
